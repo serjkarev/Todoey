186fffd3b5297079e3f5d3d2fe33744f74bcb257
skarev <sevenkss95@gmail.com>
2019-07-28 18:33:50 +0300

    Add a search bar

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -19,6 +19,7 @@ class ToDoListViewController: UITableViewController {
         super.viewDidLoad()
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         loadItems()
         
     }
@@ -80,8 +81,8 @@ class ToDoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
 
-    func loadItems(){
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+
         do{
            itemArray = try context.fetch(request)
         }catch{
@@ -91,3 +92,19 @@ class ToDoListViewController: UITableViewController {
 
 }
 
+//MARK:- Search bar methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+
+    }
+}
